0445ce264ac09b8999b10d08860d93f717b95afa
dadachen <overlooker1997@gmail.com>
2018-11-27 11:13:21 +0800

    fix some bugs

--- app/src/main/java/com/example/overl/hipe/client/LocationTransUtils.java
+++ app/src/main/java/com/example/overl/hipe/client/LocationTransUtils.java
@@ -78,7 +78,7 @@ public class LocationTransUtils {
         double cosTheta = Math.abs((mx[0] * nx[0] + mx[1] * nx[1]) / (lenMx * lenNx));
         double sinTheta = Math.sqrt(1 - cosTheta * cosTheta);
 
-        double x = cosTheta * (a - s2Old[0]) -  sinTheta * (b - s2Old[1]) + s2New[0];
+        double x = 0.9*cosTheta * (a - s2Old[0]) -  0.9*sinTheta * (b - s2Old[1]) + s2New[0];
 
         double y = -0.8 * sinTheta * (a - s2Old[0]) - 0.8 * cosTheta * (b - s2Old[1]) + s2New[1];
 
@@ -87,6 +87,6 @@ public class LocationTransUtils {
 
     public static double[] bigTransfer(double[] xy){
         double[] res = LB2xy(xy);
-        return randomTrainsfer(res[0],res[1]+500000);
+        return randomTrainsfer(res[0]-1.3,res[1]+500000);
     }
 }
